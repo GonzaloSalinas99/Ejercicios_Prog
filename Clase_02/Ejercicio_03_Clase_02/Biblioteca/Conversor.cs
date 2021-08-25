54e9e3d231f56f04a7d49901909f3c1e814a8284
GonzaloSalinas99 <fabiansalinas99@hotmail.com>
2021-08-24 22:39:36 -0300

    Resolucion de ejercicios

---
+++ Clase_02/Ejercicio_03_Clase_02/Biblioteca/Conversor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Biblioteca
+{
+    public static class Conversor
+    {
+        public static string ConvertirDecimalABinario(int numeroDecimal)
+        {
+            return Convert.ToString(numeroDecimal, 2);
+        }
+
+
+    }
+}
---
+++ Clase_02/Ejercicio_03_Clase_02/Ejercicio_03_Clase_02/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Biblioteca;
+namespace Ejercicio_03_Clase_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa numero");
+            int numero;
+            numero = int.Parse(Console.ReadLine());
+            string numeroBinario;
+
+            numeroBinario = Conversor.ConvertirDecimalABinario(numero);
+            Console.WriteLine(numeroBinario);
+        }
+    }
+}
