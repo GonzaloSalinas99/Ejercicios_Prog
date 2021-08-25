b6541d75264931774a8334626555be0833d87224
GonzaloSalinas99 <fabiansalinas99@hotmail.com>
2021-08-25 16:28:26 -0300

    .

---
+++ Clase_02/Ejercicio_04_Clase_02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ejercicio_04_Clase_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Lalo!");
+        }
+    }
+}
