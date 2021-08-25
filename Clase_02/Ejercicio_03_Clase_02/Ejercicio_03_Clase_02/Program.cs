c14f63ea6e0375d8f4da03b21bcdd911e094990f
GonzaloSalinas99 <fabiansalinas99@hotmail.com>
2021-08-24 22:57:18 -0300

    Borre linea en ejercicio 3

--- Clase_02/Ejercicio_03_Clase_02/Ejercicio_03_Clase_02/Program.cs
+++ Clase_02/Ejercicio_03_Clase_02/Ejercicio_03_Clase_02/Program.cs
@@ -10,8 +10,6 @@ namespace Ejercicio_03_Clase_02
             int numero;
             numero = int.Parse(Console.ReadLine());
             string numeroBinario;
-            int numero2=2;
-            Console.WriteLine(numero2);
             numeroBinario = Conversor.ConvertirDecimalABinario(numero)    ;
             Console.WriteLine(numeroBinario);
         }
