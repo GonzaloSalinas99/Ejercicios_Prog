ca78b674461212d10e133aeb14a738fa12ba5cc0
GonzaloSalinas99 <fabiansalinas99@hotmail.com>
2021-08-27 12:01:49 -0300

    Avance de ejercicios

---
+++ Clase_02/Biblioteca/Calculadora.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Biblioteca
+{
+    public class Calculadora
+    {
+        static int resultado;
+        public static int Calcular(int primerOperando, int segundoOperando,char operacion)
+        {
+            switch(operacion)
+            {
+                case '+':
+                        resultado = primerOperando + segundoOperando;
+                    break;
+
+                case '-':
+                    resultado = primerOperando - segundoOperando;
+                    break;
+
+                case '*':
+                    resultado = primerOperando * segundoOperando;
+                    break;
+
+                case '/':
+                    if(Validar(segundoOperando))
+                    {
+                        resultado = primerOperando / segundoOperando;
+                    }
+                    break;
+                
+            }
+            return resultado;
+        }
+
+        private static bool Validar(int segundoOperando)
+        {
+            bool respuesta=true;
+            if (segundoOperando == 0)
+            { 
+                respuesta = false;
+            }
+            return respuesta;
+        }
+
+
+    }
+}
--- Clase_02/Ejercicio_04_Clase_02/Program.cs
+++ Clase_02/Ejercicio_04_Clase_02/Program.cs
@@ -1,12 +1,67 @@
 ﻿using System;
-
+using Biblioteca;
+using System.Text;
 namespace Ejercicio_04_Clase_02
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Lalo!");
+            StringBuilder textoMenu = new StringBuilder();
+            StringBuilder textoCalculo = new StringBuilder();
+            StringBuilder textoError = new StringBuilder();
+            StringBuilder TextoUsuario = new StringBuilder();
+
+            TextoUsuario.AppendLine("Desea seguir ingresando numeros?");
+            textoMenu.AppendLine("Bienvenido a la Calculadora. Por favor ingrese dos numeros para hacer los calculos");
+            textoCalculo.AppendLine("Ingrese el signo (+) si desea sumar\n" +
+                                     "Ingrese el signo (-) si desea restar\n" +
+                                     "Ingrese el signo (*) si desea multiplicar\n" +
+                                     "Ingrese el sino (/) si desea dividir\n" +
+                                     "Su respuesta:");
+
+
+
+
+            bool primerNumero;
+            bool segundoNumero;
+
+            decimal primeroOperando;
+            decimal segundoOperando;
+
+            string respuestaUsuario;
+
+            do
+            {
+
+
+
+
+                Console.WriteLine(TextoUsuario);
+                respuestaUsuario = Console.ReadLine();
+            } while (respuestaUsuario == "si");
+
         }
     }
 }
+
+
+//Realizar un programa que permita realizar operaciones matemáticas simples
+//(suma, resta, multiplicación y división).
+
+//Crear una clase llamada Calculadora que posea dos métodos estáticos
+//(de clase):
+
+//Calcular(público): Recibirá tres parámetros, el primer operando,
+//el segundo operando y la operación matemática. El método devolverá el
+//resultado de la operación.
+
+//Validar (privado): Recibirá como parámetro el segundo operando.
+//Este método se debe utilizar sólo cuando la operación elegida sea
+//la DIVISIÓN. Este método devolverá true si el operando es distinto
+//de cero.
+
+//Se le debe pedir al usuario que ingrese dos números y la operación
+//que desea realizar (ingresando el caracter +, -, * o /).
+
+//El usuario decidirá cuándo finalizar el programa.
