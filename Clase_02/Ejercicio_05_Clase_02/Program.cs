0b9d2a0bb738eba35595cd640c8a2029222946d8
GonzaloSalinas99 <fabiansalinas99@hotmail.com>
2021-08-30 19:14:44 -0300

    nueva modificacion

---
+++ Clase_02/Ejercicio_05_Clase_02/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Ejercicio_05_Clase_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroUsuario;
+
+            Console.WriteLine("Ingresame un numero:");
+            numeroUsuario = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Cuantas veces se tiene que multiplicar??");
+            int vecesAMultiplicar = int.Parse(Console.ReadLine());
+            int multiplicacion = 1;
+            int resultado;
+            
+
+            while(multiplicacion<=vecesAMultiplicar)
+            {
+                resultado = multiplicacion * numeroUsuario;
+                Console.WriteLine($"{numeroUsuario} * {multiplicacion} = {resultado}");
+
+                multiplicacion++;
+            }
+        }
+
+
+
+    }
+}
+
+//Crear una aplicación de consola que permita al usuario ingresar un número entero.
+
+//Desarrollar un método estático que reciba un número y devuelva la tabla de multiplicación de ese número en formato string.
+
+//Se deberá utilizar la clase StringBuilder combinada con strings interpolados para armar el resultado.
+
+//Mostrar en la consola el resultado.
