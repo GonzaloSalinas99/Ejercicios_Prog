68b52b09e4702db9d39b1229b69de78eaa7067dc
GonzaloSalinas99 <fabiansalinas99@hotmail.com>
2021-08-31 20:56:44 -0300

    Nuevos ejercicios

---
+++ Clase_04/Ejercicio_01_Clase_04/Biblioteca/Sumador.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Biblioteca
+{
+    public class Sumador
+    {
+        private int cantidadSumas;
+
+        public Sumador(int cantidadSumas)
+        {
+            this.cantidadSumas = cantidadSumas;
+        }
+
+        public Sumador() : this(0)
+        {
+        }
+
+        public long Sumar(long primerOperando, long segundoOperando)
+        {
+            this.cantidadSumas++;
+            return primerOperando + segundoOperando;
+        }
+
+        public string Sumar(string primerOperando,string segundoOperando)
+        {
+            this.cantidadSumas++;
+            return primerOperando + segundoOperando;
+        }
+            
+        public static explicit operator int (Sumador sumador)
+        {
+            return sumador.cantidadSumas;
+        }
+
+        public static long operator + (Sumador sumador1,Sumador sumador2)
+        {
+            return sumador1.cantidadSumas + sumador2.cantidadSumas;
+        }
+
+
+        public static bool operator | (Sumador sumador1,Sumador sumador2)
+        {
+            return (sumador1.cantidadSumas == sumador2.cantidadSumas);
+        }
+
+    }
+}
---
+++ Clase_04/Ejercicio_01_Clase_04/Consola/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Biblioteca;
+namespace Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sumador sumador = new Sumador();
+            Sumador sumador2 = new Sumador(4);
+            long sumaLong = sumador.Sumar(21, 32);
+            Console.WriteLine(sumaLong);
+
+            string sumaString = sumador.Sumar("Hola", "Mundo");
+            Console.WriteLine(sumaString);
+
+            int numeroDeCantidadDeSumas = (int)sumador;
+            Console.WriteLine(numeroDeCantidadDeSumas);
+
+            Console.WriteLine(sumador+sumador2);
+            Console.WriteLine(sumador | sumador2);
+
+        }
+    }
+}
