002fd0954925c4554c287c81f45835a86bcde070
GonzaloSalinas99 <fabiansalinas99@hotmail.com>
2021-08-31 18:48:18 -0300

    Nuevos Ejercicios

---
+++ Clase_03/Ejercicio_A01_Clase_03/Biblioteca/Conductores.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Biblioteca
+{
+    public class Conductores
+    {
+        private string nombre;
+        private int[] KilometroPorDia;
+
+        public Conductores(string nombre,int [] kilometrosRecorridos)
+        {
+            this.nombre = nombre;
+            this.KilometroPorDia = kilometrosRecorridos;
+
+        }
+
+        public string GetNombre()
+        {
+            return this.nombre;
+        }
+        public string GetKilometros()
+        {
+            return $"\nDia 1: {KilometroPorDia[0]} \n Dia 2: {KilometroPorDia[1]} \n Dia 3: {KilometroPorDia[2]} \n Dia 4: {KilometroPorDia[3]} \n" +
+                $"Dia 5: {KilometroPorDia[4]} \n Dia 6: {KilometroPorDia[5]} \n Dia 7: {KilometroPorDia[6]} \n";
+        
+        }
+        public string Mostrar()
+        {
+            return $"Nombre Conductor: {GetNombre()} Kilometros Recorridos: {GetKilometros()}";
+        }
+
+        public int TotalKmRecorridos()
+        {
+            return KilometroPorDia[0] + KilometroPorDia[1] + KilometroPorDia[2] + KilometroPorDia[4];
+        }
+
+        public string nombreMayorKmRecorridos(Conductores[] listaConductores)
+        {
+            int mayorKmRecorrido = 0;
+            string nombreMayorKmRecorridos="";
+
+            for (int i = 0; i < listaConductores.Length; i++)
+            {
+                if (i == 0)
+                { 
+                     nombreMayorKmRecorridos = listaConductores[i].GetNombre();
+                    mayorKmRecorrido = listaConductores[i].TotalKmRecorridos();
+                }
+                else
+                {
+                    if (mayorKmRecorrido < listaConductores[i].TotalKmRecorridos())
+                    {
+                        mayorKmRecorrido = listaConductores[i].TotalKmRecorridos();
+                        nombreMayorKmRecorridos = listaConductores[i].GetNombre();
+                    }
+                }
+            }
+            return nombreMayorKmRecorridos;
+        }
+
+        public int GetKmEnDia(int diaABuscar)
+        {
+            return KilometroPorDia[diaABuscar - 1]; 
+        }
+
+
+    }
+}
---
+++ Clase_03/Ejercicio_A01_Clase_03/Consola/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Biblioteca;
+namespace Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conductores[] listaConductores = new Conductores[2];
+            
+            for(int i=0;i<listaConductores.Length;i++)
+            {
+                Console.WriteLine("Ingrese el nombre del conductor:");
+                string nombreConductor = Console.ReadLine();
+
+                int[] kmRecorridos = new int[7];
+                int km;
+                for(int j=0;j<kmRecorridos.Length;j++)
+                {
+                    if (j==0||j==1||j==2||j==4)
+                    {
+                        Console.WriteLine($"Ingrese el km recorrido en el dia {j + 1}");
+                        if (int.TryParse(Console.ReadLine(), out km))
+                        {
+                            kmRecorridos[j] = km;
+                        }
+                        else
+                        {
+                            Console.WriteLine("No se pudo guardar el km");
+                        }
+                    }
+                    else
+                    {
+                        kmRecorridos[j] = 0;
+                    }
+                }
+                listaConductores[i] = new Conductores(nombreConductor, kmRecorridos);
+            }
+
+
+
+            int mayorKmEnUnDia=0;
+            int diaABuscar;
+            string nombreConductorMasKm="";
+            Console.WriteLine("Ingrese el dia a buscar para saber el conductor con mas KM");
+            diaABuscar = int.Parse(Console.ReadLine());
+            for(int i=0;i<listaConductores.Length;i++)
+            {
+                if(i==0)
+                {
+                    mayorKmEnUnDia = listaConductores[i].GetKmEnDia(diaABuscar);
+                    nombreConductorMasKm = listaConductores[i].GetNombre();
+                }
+                else
+                {
+                    if(mayorKmEnUnDia<listaConductores[i].GetKmEnDia(diaABuscar))
+                    {
+                        mayorKmEnUnDia = listaConductores[i].GetKmEnDia(diaABuscar);
+                        nombreConductorMasKm = listaConductores[i].GetNombre();
+
+                    }
+                }
+            }
+
+            Console.WriteLine($"Nombre del conductor que mas viajo el Dia {diaABuscar} de la semana: {nombreConductorMasKm}");
+
+
+            foreach(Conductores conductor in listaConductores)
+            {
+                Console.WriteLine(conductor.Mostrar());
+                Console.WriteLine(conductor.nombreMayorKmRecorridos(listaConductores));
+            }
+
+
+        }
+    }
+}
+
+
+//Consigna#
+//De una empresa de transporte de cargas se quiere guardar el nombre de los conductores y los kilómetros que conducen cada día de la semana.
+
+//Para guardar esta información, la empresa de transporte tendrá un array de conductores.
+
+//De cada conductor se tendrá la siguiente información:
+
+//Nombre del conductor.
+//Kilometros recorridos por cada día de la semana. Por ejemplo:
+//Día 1: 200
+//Día 2: 599
+//Día 3: 899
+//Día 4: 0(tiene franco)
+//Día 5: 256
+//Día 6: 0
+//Día 7: 0
+//Desarrollar las clases y métodos necesarios. Toda la lógica de negocio deberá estar encapsulada en clases y dentro de un proyecto de tipo biblioteca de clases.
+
+//Crear un proyecto de consola y en el método Main cargar 3 conductores a la empresa de transportes.
+
+//Luego mostrar:
+
+//El conductor que hizo más km en esa semana.
+//El conductor que hizo más km el día 3.
+//El conductor que hizo más km el día 5.
