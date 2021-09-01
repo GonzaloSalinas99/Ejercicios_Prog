73ff45ef8d2e7d81867fcbe2c1cffe57fba2d9ed
GonzaloSalinas99 <fabiansalinas99@hotmail.com>
2021-08-31 21:00:34 -0300

    Modificacion

---
+++ Clase_04/Ejercicio_02_Clase_04/Biblioteca/Producto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Biblioteca
+{
+    public class Producto
+    {
+        private string codigoDeBarra;
+        private string marca;
+        private float precio;
+
+
+
+
+
+    }
+}
